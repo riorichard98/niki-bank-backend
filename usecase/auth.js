e6a2f847fc357374b8a6636684b9ea782aea7798
riorichard98 <riorichard12@gmail.com>
2024-06-23 15:01:33 +0700

    very long time jwt expire

--- usecase/auth.js
+++ usecase/auth.js
@@ -89,7 +89,7 @@ const login = async (req, res) => {
 
         // If email and password are correct, generate JWT token
         const userId = result.rows[0].id;
-        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '10y' });
 
         res.status(200).json({ message: 'Login berhasil', userId, token });
 
